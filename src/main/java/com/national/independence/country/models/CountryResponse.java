f5b6c8283713e8854256372e1a163c2664b509ba
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-18 14:24:45 -0600

    model Country
    response

---
+++ src/main/java/com/national/independence/country/models/CountryResponse.java
@@ -0,0 +1,55 @@
+package com.national.independence.country.models;
+
+
+public class CountryResponse {
+    
+    private String countryName;
+    private String capitalName;
+    private String independenceDate;
+    private int yearsOfIndependency;
+    private int dayssOfIndependency;
+
+    
+
+    public String getCountryName() {
+        return this.countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCapitalName() {
+        return this.capitalName;
+    }
+
+    public void setCapitalName(String capitalName) {
+        this.capitalName = capitalName;
+    }
+
+    public String getIndependenceDate() {
+        return this.independenceDate;
+    }
+
+    public void setIndependenceDate(String independenceDate) {
+        this.independenceDate = independenceDate;
+    }
+
+    public int getYearsOfIndependency() {
+        return this.yearsOfIndependency;
+    }
+
+    public void setYearsOfIndependency(int yearsOfIndependency) {
+        this.yearsOfIndependency = yearsOfIndependency;
+    }
+
+    public int getDayssOfIndependency() {
+        return this.dayssOfIndependency;
+    }
+
+    public void setDayssOfIndependency(int dayssOfIndependency) {
+        this.dayssOfIndependency = dayssOfIndependency;
+    }
+
+
+}
