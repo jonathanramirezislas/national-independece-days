ca3e6f41a7744969ab0ff7798602b5b214e3dcf0
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-18 15:26:53 -0600

    import data to H2

--- src/main/java/com/national/independence/country/CountryApplication.java
+++ src/main/java/com/national/independence/country/CountryApplication.java
@@ -8,6 +8,7 @@ public class CountryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountryApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/national/independence/country/controller/IndependencyController.java
+++ src/main/java/com/national/independence/country/controller/IndependencyController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.Period;
 import java.util.Optional;
 
-/**
- * Author: VIP
- */
+
 @RestController()
 public class IndependencyController {
 
